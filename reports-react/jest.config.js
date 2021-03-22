ae05814cb8a6030ea3e2f3989ad832a6686d66db
Adam Tombleson <rekarnar@gmail.com>
2021-03-22 20:15:02 +0100

    add jest config

--- reports-react/jest.config.js
+++ reports-react/jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   testMatch: [
     "**/__tests__/**/*.test.[jt]s?(x)"
+  ],
+  testPathIgnorePatterns: [
+    "reports-react/src/processing/utils/__tests__/zScore.test.ts"
   ]
 };
