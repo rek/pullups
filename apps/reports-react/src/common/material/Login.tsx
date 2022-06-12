4733d587df800092afe5aeef018a0f3b9dc9caf4
Adam Tombleson <rekarnar@gmail.com>
2022-06-12 13:11:22 +0200

    feat: login button style

--- apps/reports-react/src/common/material/Login.tsx
+++ apps/reports-react/src/common/material/Login.tsx
@@ -1,49 +1,49 @@
-import * as React from "react"
-import styled from "styled-components"
+import * as React from "react";
+import styled from "styled-components";
 
-import { Button } from "./Button"
-import { Input } from "./Input"
+import { Button } from "./Button";
+import { Input } from "./Input";
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-content: center;
   flex-direction: column;
-`
+`;
 const ErrorWrapper = styled.div`
   margin: 10px;
   max-width: 50vh;
   color: red;
-`
+`;
 const InputWrapper = styled(Input)`
   margin: 10px;
   max-width: 50vh;
-`
+`;
 const ButtonWrapper = styled(Button)`
   margin: 20px;
-  margin-top: 10px;
+  margin-top: 11px;
   margin-left: 10px;
   max-width: 50vh;
-`
+`;
 
 export interface LoginProps {
   error?: string;
   setCredentials: (props: { email: string; password: string }) => unknown;
 }
 export const Login: React.FC<LoginProps> = ({ setCredentials, error }) => {
-  const [email, setEmail] = React.useState("")
-  const [password, setPassword] = React.useState("")
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
   const handleLogin = () => {
     // console.log("Setting:", { email, password });
-    setCredentials({ email, password })
-  }
+    setCredentials({ email, password });
+  };
   const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value)
-  }
+    setEmail(event.target.value);
+  };
   const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
 
   return (
     <form className="form">
@@ -59,5 +59,5 @@ export const Login: React.FC<LoginProps> = ({ setCredentials, error }) => {
         <ButtonWrapper onClick={handleLogin}>Log in</ButtonWrapper>
       </Wrapper>
     </form>
-  )
-}
+  );
+};
